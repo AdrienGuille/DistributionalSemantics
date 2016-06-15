66559a46570bd4528eb5d6b86b6c29da0316ceb0
Adrien Guille <adrien.guille@univ-lyon2.fr>
2016-06-15 10:58:08 +0200

    Add option to build the cooccurrence matrix with a decreasing weighting function

--- structure/corpus.py
+++ structure/corpus.py
@@ -17,17 +17,18 @@ def tokenize(text):
 
 
 class Corpus:
-    def __init__(self, source_file_path, max_nb_features=5000, window_size=5):
+    def __init__(self, source_file_path, max_nb_features=10000, window_size=5, decreasing_weighting=False):
         input_file = open(source_file_path)
         csv_reader = csv.reader(input_file, delimiter='\t')
         header = next(csv_reader)
         text_column_index = header.index('text')
-        print('Loading corpus...')
 
         # first pass to identify features, i.e. the vocabulary
-
+        print('   Identifying features (i.e. the vocabulary)...')
+        self.size = 0
         word_frequency = {}
         for line in csv_reader:
+            self.size += 1
             words = tokenize(line[text_column_index])
             # update word frequency
             for word in words:
@@ -47,10 +48,11 @@ class Corpus:
             feature = vocabulary[i][0]
             self.vocabulary.append(feature)
             self.vocabulary_map[feature] = i
-        print('   Vocabulary size: %d' % len(self.vocabulary))
+        print('      Vocabulary size: %d' % len(self.vocabulary))
 
         # second pass to compute the co-occurrence matrix
 
+        print('   Computing X (i.e. the co-occurrence frequency matrix)...')
         self.X = dok_matrix((len(self.vocabulary), len(self.vocabulary)), dtype=np.short)
         # go back to the beginning of the csv file
         input_file.seek(0)
@@ -69,15 +71,31 @@ class Corpus:
                     end = i + window_size
                     if end >= nb_words:
                         end = nb_words - 1
-                    context_0 = words[start:i]
-                    context_1 = words[i + 1:end + 1]
-                    context_0.extend(context_1)
-                    for word in context_0:
-                        if self.vocabulary_map.get(word) is not None:
-                            column_index = self.vocabulary_map[word]
+                    # scan left context
+                    context_left = words[start:i]
+                    for j in range(0, len(context_left)):
+                        if self.vocabulary_map.get(context_left[j]) is not None:
+                            column_index = self.vocabulary_map[context_left[j]]
                             # update co-occurrence count
                             count = 0
+                            weight = 1
+                            if decreasing_weighting:
+                                weight = len(context_left) - j
                             if (row_index, column_index) in self.X:
                                 count = self.X[row_index, column_index]
-                            self.X[row_index, column_index] = count + 1
-        print('   Number of non-zero entries: %d' % self.X.getnnz())
+                            self.X[row_index, column_index] = count + 1 / weight
+                    # scan right context
+                    context_right = words[i + 1:end + 1]
+                    for j in range(0, len(context_right)):
+                        if self.vocabulary_map.get(context_left[j]) is not None:
+                            column_index = self.vocabulary_map[context_left[j]]
+                            # update co-occurrence count
+                            count = 0
+                            weight = 1
+                            if decreasing_weighting:
+                                weight = j
+                            if (row_index, column_index) in self.X:
+                                count = self.X[row_index, column_index]
+                            self.X[row_index, column_index] = count + 1 / weight
+        print('      Number of non-zero entries: %d' % self.X.getnnz())
+        self.X = self.X.tocsr()
